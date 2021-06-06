59e472af0cb94cc8327c6d155441d2b9e6c600aa
Thatz Weerakoon <59426115+thatz98@users.noreply.github.com>
2021-06-06 15:08:46 +0530

    added spacing common component

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -12,6 +12,7 @@ import FeaturedOrganizations from './components/FeaturedOrganizations'
 import FeaturedJobs from './components/FeaturedJobs'
 import PostJobSection from './components/PostJobSection'
 import Footer from '../components/Footer'
+import Space from '../components/Space'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,7 +21,12 @@ const useStyles = makeStyles((theme) => ({
         minHeight: '100vh'
     },
     container: {
-        paddingTop: 20,
+        width: '100%',
+        padding: 5,
+        paddingTop: 10,
+        [theme.breakpoints.down('md')]: {
+            paddingTop: 10,
+        },
         [theme.breakpoints.down('xs')]: {
             paddingTop: 0,
         },
@@ -46,44 +52,50 @@ function Home() {
 
     return (
         <div className={classes.root}>
-                <div className="overlay">
-                    <React.Fragment>
-                        <CssBaseline />
-                        <Container maxWidth="false" className={classes.container}>
-                            <Grid container direction="row" spacing={3} style={{margin:'0px'}} className={classes.topBarGrid}>
-                                <Grid item xs={0} sm={4} md={3} lg={2} style={{ position: 'fixed' }} className={classes.sideDrawerGrid}>
-                                    <SideDrawer />
+            <div className="overlay">
+                <React.Fragment>
+                    <CssBaseline />
+                    <Container maxWidth="false" className={classes.container}>
+                        <Grid container direction="row" spacing={3} style={{ margin: '0px', width: '100%' }} className={classes.topBarGrid}>
+                            <Grid item xs={0} sm={4} md={3} lg={2} style={{ position: 'fixed' }} className={classes.sideDrawerGrid}>
+                                <SideDrawer />
+                            </Grid>
+                            <Grid item xs={0} sm={4} md={3} lg={2} className={classes.sideDrawerGrid}></Grid>
+                            <Grid item container xs={12} sm={8} md={9} lg={10} spacing={0} className={classes.topBarGrid}>
+                                <Grid item sm={12}>
+                                    <Topbar />
                                 </Grid>
-                                <Grid item xs={0} sm={4} md={3} lg={2} className={classes.sideDrawerGrid}></Grid>
-                                <Grid item container xs={12} sm={8} md={9} lg={10} spacing={3} className={classes.topBarGrid}>
-                                    <Grid item sm={12}>
-                                        <Topbar />
-                                    </Grid>
+                                <Space value={16} />
 
+                                <Grid item sm={12}>
+                                    <FloatCard>
+                                        <HeroSection />
+                                    </FloatCard>
+                                </Grid>
+
+                                <Space value={16} />
+
+                                <Grid item xs={12} md={6}>
+                                    <FeaturedJobs />
+                                </Grid>
+                                <Space value={16} />
+                                <Grid item container xs={12} md={6}>
                                     <Grid item sm={12}>
-                                        <FloatCard>
-                                            <HeroSection />
-                                        </FloatCard>
+                                        <PostJobSection />
                                     </Grid>
-                                    <Grid item xs={12} md={6}>
-                                        <FeaturedJobs />
+                                    <Space value={16} />
+                                    <Grid item sm={12} className={classes.FeaturedOrganizations}>
+                                        <FeaturedOrganizations />
                                     </Grid>
-                                    <Grid item container xs={12} md={6}>
-                                        <Grid item sm={12}>
-                                            <PostJobSection />
-                                        </Grid>
-                                        <Grid item sm={12} className={classes.FeaturedOrganizations}>
-                                            <FeaturedOrganizations />
-                                        </Grid>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <Footer />
-                                    </Grid>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Footer />
                                 </Grid>
                             </Grid>
-                        </Container>
-                    </React.Fragment>
-                </div>
+                        </Grid>
+                    </Container>
+                </React.Fragment>
+            </div>
         </div>
     )
 }
