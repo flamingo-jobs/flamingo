63bbb953a96e9b70cdfc162da4251b870b794ed8
Isurika1998 <2018is055@stu.ucsc.cmb.ac.lk>
2021-06-15 16:13:09 +0530

    awards view, add

---
+++ client/src/employee/components/Awards.js
@@ -0,0 +1,247 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import FloatCard from '../../components/FloatCard';
+import theme from '../../Theme';
+import EditIcon from '@material-ui/icons/Edit';
+import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
+import Grid from '@material-ui/core/Grid';
+import AwardItem from './AwardItem';
+import BACKEND_URL from '../../Config';
+import Fade from '@material-ui/core/Fade';
+import Divider from '@material-ui/core/Divider';
+import CloseIcon from '@material-ui/icons/Close';
+import AddIcon from '@material-ui/icons/Add';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles({
+  defaultButton: {
+    backgroundColor: theme.palette.stateBlue,
+    color: theme.palette.white,
+    "&:hover": {
+      backgroundColor: '#0088cc',
+      color: 'white',
+    }
+  },
+  editIcon: {
+    "&:hover": {
+      fontSize: "30px",
+    }
+  },
+  closeIcon: {
+    "&:hover": {
+      fontSize: "25px",
+      color: "#b30000 !important"
+    }
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    width: '600px',
+    borderRadius: 10,
+    paddingBottom: "30px"
+  },
+  form: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '5% 15% 5% 15%'
+  },
+  field: {
+    margin: "20px 0px 20px 0px",
+    display: "flex",
+    fontSize: "16px",
+    "& label": {
+      color: "#777",
+      fontSize: '16px',
+    }
+  }
+});
+
+function Achievements() {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [award, setAward] = useState(null);
+  const [state, setState] = useState({title: null, issuedBy: null, date: null, description: null});
+
+  useEffect(()=>{
+    axios.get(`${BACKEND_URL}/jobseeker/60c5f2e555244d11c8012480`)
+    .then(res => {
+      if(res.data.success){
+        setAward(res.data.jobseeker.award)
+      }
+    })
+  },[])
+
+  function handleOpen(){
+    setOpen(true);
+  }
+
+  function handleClose(){
+    setOpen(false);
+  }
+
+  //---------------------------- text field onChange events
+  function onChangeTitle(e){
+    setState(prevState => {
+      return {...prevState, title: e.target.value}
+    })
+  }
+
+  function onChangeIssuedBy(e){
+    setState(prevState => {
+      return {...prevState, issuedBy: e.target.value}
+    })
+  }
+
+  function onChangeDate(e){
+    setState(prevState => {
+      return {...prevState, date: e.target.value}
+    })
+  }
+
+  function onChangeDescription(e){
+    setState(prevState => {
+      return {...prevState, description: e.target.value}
+    })
+  }
+
+  function onSubmit(e){
+    e.preventDefault();
+    const newAward = {
+        title: state.title,
+        issuedBy: state.issuedBy,
+        date: state.date,
+        description: state.description,
+    }
+
+    axios.put(`${BACKEND_URL}/jobseeker/addAward/60c5f2e555244d11c8012480`,newAward)
+    .then(res => console.log(newAward));
+    handleClose();
+  }
+  
+  const displayAwardFields = () => {
+    if (award) {
+      if (award.length > 0) {
+      return award.map(awd => (
+            <AwardItem title={awd.title} issuedBy={awd.issuedBy} date={awd.date} description={awd.description} />
+            ))
+      }else{
+        return (<Typography variant="body2" color="textSecondary" component="p">Award details not added.</Typography>)
+      }
+    }else{
+      return (<Typography variant="body2" color="textSecondary" component="p">Award details not added.</Typography>)
+    }
+  }
+
+  return (
+    <FloatCard>
+      <Grid container spacing={3}>
+        <Grid item xs style={{ textAlign: 'left',}}>
+            <Typography gutterBottom variant="h5" style={{color: theme.palette.tuftsBlue,padding:'10px',fontWeight:'bold'}}>
+                <EmojiEventsIcon style={{color: theme.palette.turfsBlue,marginRight: '10px',marginBottom:'-5px',fontSize:'27'}}/>
+                Awards
+            </Typography>
+        </Grid>
+        <Grid item style={{ textAlign: 'right' }}>
+            <Button className={classes.defaultButton} style={{ float: 'right',marginRight: '0px',backgroundColor:'white'}} onClick={handleOpen}>
+                <AddIcon style={{color: theme.palette.tuftsBlue,}} className={classes.editIcon} />
+            </Button>
+        </Grid>
+
+        {/*-------------- add new edu field popup content ------------------- */}
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <div style={{paddingTop:'40px'}}>
+                <Grid container xs={12} direction="row">
+                  <Grid item xs={10}>
+                    <Typography gutterBottom variant="h5" style={{textAlign:'center',paddingLeft:'50px',color:theme.palette.stateBlue}}>
+                      Add Education
+                    </Typography>
+                    <Divider variant="middle" style={{marginLeft:'100px'}} />
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Button className={classes.defaultButton} style={{ float: 'right',marginRight:'10px',marginTop:'-20px',backgroundColor:'white'}} onClick={handleClose}>
+                      <CloseIcon className={classes.closeIcon} style={{color: '#666',}} />
+                    </Button>
+                  </Grid>
+                </Grid>
+              </div>
+              <form className={classes.form} onSubmit={onSubmit}>
+                <div>
+                <TextField
+                  className={classes.field}
+                    id="outlined-basic"
+                    label="Title"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    onChange={onChangeTitle}
+                  />
+                  <TextField
+                  className={classes.field}
+                    id="outlined-basic"
+                    label="Issued By"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    onChange={onChangeIssuedBy}
+                  />
+                  <TextField
+                  className={classes.field}
+                    id="outlined-basic"
+                    label="Date"
+                    type="number"
+                    variant="outlined"
+                    size="small"
+                    onChange={onChangeDate}
+                    style={{width:'30%'}}
+                  />
+                  <TextField
+                    className={classes.field}
+                    id="outlined-multiline-static"
+                    label="Description"
+                    multiline
+                    rows={5}
+                    variant="outlined"
+                    onChange= {onChangeDescription}
+                  />
+                  </div>
+                  <Button type="submit" className={classes.defaultButton} style={{ width:'100%',marginTop:'5%'}}>Apply Changes</Button>
+              </form>
+              
+            </div>
+          </Fade>
+        </Modal>
+        
+      </Grid>
+      <Grid container spacing={3}>
+      <Grid item xs={12}>
+                {displayAwardFields()}
+            </Grid>
+        </Grid>
+    </FloatCard>
+  );
+}
+
+export default Achievements
