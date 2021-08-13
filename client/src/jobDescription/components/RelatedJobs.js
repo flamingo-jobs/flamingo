5d79eaf8e6d223b22d4a9edcfcb415892f51f8fc
yeshanGune <gunawardanayeshan@gmail.com>
2021-08-13 09:07:46 +0530

    related job issue

--- client/src/jobDescription/components/RelatedJobs.js
+++ client/src/jobDescription/components/RelatedJobs.js
@@ -8,6 +8,7 @@ import ArrowForwardRoundedIcon from '@material-ui/icons/ArrowForwardRounded';
 import theme from '../../Theme';
 import BACKEND_URL from '../../Config';
 import { Link } from 'react-router-dom';
+const jwt = require("jsonwebtoken");
 
 
 const useStyles = makeStyles((theme) => ({
@@ -67,6 +68,13 @@ function RelatedJobs(props) {
 
     const isSignedIn = sessionStorage.getItem( "userToken" ) ? true : false;
 
+    const token = sessionStorage.getItem("userToken");
+    const [role, setRole] = useState(
+        jwt.decode(token, { complete: true })
+          ? jwt.decode(token, { complete: true }).payload.userRole
+          : null
+    );
+
     useEffect(() => {
         retrieveJobseeker();
       }, []);
@@ -122,7 +130,10 @@ function RelatedJobs(props) {
     }, [relatedJobs])
 
     const displayRelatedJobs = () => {
-        if (!isSignedIn || (isSignedIn && relatedJobs && savedJobIds !== "empty")) {
+        if (!isSignedIn || 
+            (isSignedIn && role === "jobseeker" && relatedJobs && savedJobIds !== "empty") ||
+            (isSignedIn && role === "admin" && relatedJobs) || 
+            (isSignedIn && role === "employer" && relatedJobs)) {
             return relatedJobs.map(featuredJob => (
                 <Grid item sm={12} key={featuredJob._id} className={classes.jobGridCard}>
                     <JobCard 
