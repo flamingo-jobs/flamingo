b213f9d70ed5b588d0d241b05b5b5d6949fd3ad6
Udara Weerasinghe <64691719+udara-kw@users.noreply.github.com>
2021-08-25 02:45:24 +0530

    Add settings when employer initiation

--- server/controllers/settingsController.js
+++ server/controllers/settingsController.js
@@ -1,72 +1,111 @@
-const Settings = require('../models/settings');
+const Settings = require("../models/settings");
 
+const create = async (req, res) => {
+  const { loginId } = req.body;
+  let shortlistingSettings = await Settings.find().or([
+    { type: "shortlistingDefaults" },
+    { type: "shortlistingExperienceDefaults" },
+    { type: "shortlistingEducationDefaults" },
+  ]);
 
-const getAll = (req,res) => {
-    Settings.find().exec((err,settings) => {
-        if(err){
-            return res.status(400).json({
-                error: err
-            })
-        }
-        return res.status(200).json({
-            success: true,
-            existingData: settings
-        });
-    });
-}
-
-const getById = (req,res) => {
-    Settings.findById(req.params.id).exec((err,settings) => {
-        if(err){
-            return res.status(400).json({
-                error: err
-            })
-        }
-        return res.status(200).json({
-            success: true,
-            existingData: settings
-        });
-    });
-}
-
-const getByType = (req,res) => {
-    Settings.find({'type': req.params.typeString}).exec((err,settings) => {
-        if(err){
-            return res.status(400).json({
-                error: err
-            })
-        }
-        return res.status(200).json({
-            success: true,
-            existingData: settings
-        });
-    });
-}
-
-
-const update = (req,res) => {
-
-    Settings.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set:req.body
-        },
-        (err,settings) =>{
-            if(err){
-                return res.status(400).json({
-                    error:err
-                })
-            }
-            return res.status(200).json({
-                success: "Updated successfully"
-            });
-        }
+  let newSettings = [];
+  let errorList = [];
+  await shortlistingSettings.map((x) => {
+    x.type = x.type.replace("Defaults", "-" + loginId);
+    newSettings.push(
+      new Settings({
+        type: x.type,
+        settings: x.settings,
+        tag: x.tag,
+      })
     );
-}
+  });
+
+  if (newSettings.length) {
+    newSettings.forEach((element) => {
+      element
+        .save()
+        .then((res) => {})
+        .catch((err) => {
+          if (err) errorList.push(err);
+        });
+    });
+  }
+  if (errorList.length)
+    return res
+      .status(500)
+      .json({ error: "settings-save-failed", errorAt: errorList.length });
+
+  return res.status(200).json({
+    success: true,
+  });
+};
+
+const getAll = (req, res) => {
+  Settings.find().exec((err, settings) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingData: settings,
+    });
+  });
+};
+
+const getById = (req, res) => {
+  Settings.findById(req.params.id).exec((err, settings) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingData: settings,
+    });
+  });
+};
+
+const getByType = (req, res) => {
+  Settings.find({ type: req.params.typeString }).exec((err, settings) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingData: settings,
+    });
+  });
+};
+
+const update = (req, res) => {
+  Settings.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (err, settings) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+      return res.status(200).json({
+        success: "Updated successfully",
+      });
+    }
+  );
+};
 
 module.exports = {
-    getByType,
-    getAll,
-    getById,
-    update,
-}
+  create,
+  getByType,
+  getAll,
+  getById,
+  update,
+};
