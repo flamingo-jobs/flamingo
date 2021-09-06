9fefa8bb8f913e0047d2e82a23dc7f6fa65e0da8
Udara Weerasinghe <64691719+udara-kw@users.noreply.github.com>
2021-09-06 17:42:24 +0530

    BR upload ui

--- client/src/employer/Settings/VerificationSettings.js
+++ client/src/employer/Settings/VerificationSettings.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Box,
   Button,
@@ -12,8 +12,11 @@ import {
   makeStyles,
   TextField,
   Typography,
+  CircularProgress,
 } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import PublishIcon from "@material-ui/icons/Publish";
+import DescriptionIcon from "@material-ui/icons/Description";
 import SnackBarAlert from "../../components/SnackBarAlert";
 import BACKEND_URL from "../../Config";
 import axios from "axios";
@@ -61,6 +64,14 @@ const useStyles = makeStyles((theme) => ({
   dialogbuttons: {
     color: "red",
   },
+  submitBtnWrapper: {
+    marginTop: "2em",
+  },
+  fileNameContainer: {
+    marginTop: "5px",
+    display: "flex",
+    justifyContent: "center",
+  },
 }));
 
 const VerificationSettings = (props) => {
@@ -89,68 +100,62 @@ const VerificationSettings = (props) => {
     setAlertShow(false);
   };
 
-  const [deletePassword, setDeletePassword] = useState("");
-  const [open, setOpen] = React.useState(false);
-  const handleClickOpen = (e) => {
-    e.preventDefault();
-    const userId = jwt.decode(sessionStorage.getItem("userToken"), {
-      complete: true,
-    }).payload.userId;
-    axios
-      .post(`${BACKEND_URL}/api/check-password`, {
-        userId: userId,
-        password: deletePassword,
-      })
-      .then((res) => {
-        if (res.data.success) {
-          setOpen(true);
-        } else {
-          setAlertData({
-            severity: "error",
-            msg: "Password you have entered does not match with your account password!",
-          });
-          handleAlert();
-        }
-      });
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const deleteUser = async (e) => {
-    e.preventDefault();
-    const userData = jwt.decode(sessionStorage.getItem("userToken"), {
-      complete: true,
-    }).payload;
-    const deleteUserData = {
-      userId: userData.userId,
-      loginId: sessionStorage.getItem("loginId"),
-      role: userData.userRole,
-      accessTokens: userData.accessTokens,
+  const timer = useRef();
+  useEffect(() => {
+    return () => {
+      clearTimeout(timer.current);
     };
-    axios
-      .post(`${BACKEND_URL}/api/remove-user`, deleteUserData)
-      .then((res) => {
-        if (res.data.success) {
-          sessionStorage.clear();
-          localStorage.clear();
-          window.location = "/";
-        } else {
-          setAlertData({
-            severity: "error",
-            msg: "Failed to delete account!",
-          });
-          handleAlert();
-        }
-      })
-      .catch((err) => {
-        if (err) {
-          setAlertData({
-            severity: "error",
-            msg: "Failed to delete account!",
-          });
-          handleAlert();
-        }
+  }, []);
+
+  const [loading, setLoading] = React.useState(false);
+  const [fileData, setFileData] = useState("empty");
+  const [success, setSuccess] = React.useState(false);
+  const displayFileName = () => {
+    if (fileData === "empty") {
+      return null;
+    }
+    return (
+      <div className={classes.fileNameContainer}>
+        <DescriptionIcon className={classes.fileIcon}></DescriptionIcon>
+        <div className={classes.fileName}>{fileData.name}</div>
+      </div>
+    );
+  };
+  const handleFileChange = (e) => {
+    setFileData("empty");
+
+    if (e.target.files[0]) {
+      let nameSplit = e.target.files[0].name.split(".");
+      if (nameSplit[nameSplit.length - 1] !== "pdf") {
+        setAlertData({
+          severity: "error",
+          msg: "Invalid file type, only PDF file type is allowed",
+        });
+        handleAlert();
+      } else {
+        setFileData(e.target.files[0]);
+      }
+    }
+  };
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    if (fileData === "empty") {
+      setAlertData({
+        severity: "error",
+        msg: "You should upload your resume first",
       });
+      handleAlert();
+      return;
+    }
+
+    if (!loading && fileData !== "empty") {
+      setSuccess(false);
+      setLoading(true);
+      timer.current = window.setTimeout(() => {
+        setSuccess(true);
+        setLoading(false);
+      }, 1000);
+    }
   };
 
   return (
@@ -173,117 +178,115 @@ const VerificationSettings = (props) => {
           spacing={2}
           direction="column"
         >
-          <form onSubmit={handleClickOpen}>
-            <Grid item xs={12}>
-              <Grid item xs={12} md={12} lg={6} align="left">
-                <Grid container spacing={3}>
-                  {props.message === "rejected" ? (
-                    <Grid item xs={12} align="left">
-                      <Typography variant="h5" color="error">Verification Failed!</Typography>
-                      <Typography>
-                        Document you have provided has failed to verify your
-                        business. Please provide a valid document which can
-                        prove your business. If you think this is a mistake,
-                        please contact our support center.
-                      </Typography>
-                    </Grid>
-                  ) : (
-                    <Grid item xs={12} align="left">
-                      <Typography variant="h5">Verify Account</Typography>
-                      <Typography>
-                        Please provide your business registration document or
-                        any other relevant document to verify your company. You
-                        will receive the verified badge{" "}
-                        <VerifiedUserIcon
-                          color="primary"
-                          className={classes.verifiedBadge}
-                        />{" "}
-                        after your name inside Flamingo.com when our support
-                        center verifies your business.
-                      </Typography>
-                    </Grid>
-                  )}
-
+          <Grid item xs={12}>
+            <Grid item xs={12} md={12} lg={6} align="left">
+              <Grid container spacing={3}>
+                {props.message === "rejected" ? (
                   <Grid item xs={12} align="left">
-                    <TextField
-                      label="Password"
-                      name="deletePassword"
-                      type="password"
-                      value={deletePassword}
-                      onChange={(e) => setDeletePassword(e.target.value)}
-                      size="small"
-                      variant="outlined"
-                      required
-                    />
+                    <Typography variant="h5" color="error">
+                      Verification Failed!
+                    </Typography>
+                    <Typography>
+                      Document you have provided has failed to verify your
+                      business. Please provide a valid document which can prove
+                      your business. If you think this is a mistake, please
+                      contact our support center.
+                    </Typography>
                   </Grid>
-                </Grid>
-              </Grid>
-              {/* Submit Buttons */}
-              <Grid item xs={12}>
-                <Grid
-                  item
-                  container
-                  xs={12}
-                  className={classes.footer}
-                  alignItems="left"
-                  justify="left"
-                  spacing={3}
-                >
-                  <Grid
-                    item
-                    container
-                    md={6}
-                    className={classes.actions}
-                    spacing={2}
+                ) : (
+                  <Grid item xs={12} align="left">
+                    <Typography variant="h5">Verify Account</Typography>
+                    <Typography>
+                      Please provide your business registration document or any
+                      other relevant document to verify your company. You will
+                      receive the verified badge{" "}
+                      <VerifiedUserIcon
+                        color="primary"
+                        className={classes.verifiedBadge}
+                      />{" "}
+                      after your name inside Flamingo.com when our support
+                      center verifies your business.
+                    </Typography>
+                  </Grid>
+                )}
+
+                <Grid item xs={12} align="left">
+                  <form
+                    onSubmit={handleFormSubmit}
+                    encType="multipart/form-data"
                   >
-                    <Grid item>
-                      <Button
-                        fullWidth
-                        type="submit"
-                        className={classes.button}
+                    <Grid
+                      item
+                      container
+                      md={12}
+                      className={classes.actions}
+                      spacing={2}
+                    >
+                      <Grid item>
+                        <input
+                          className={classes.input}
+                          id="resume"
+                          name="resume"
+                          type="file"
+                          accept="application/pdf"
+                          hidden
+                          onChange={handleFileChange}
+                        />
+                        <label htmlFor="resume">
+                          <Button
+                            variant="outlined"
+                            color="primary"
+                            component="span"
+                            startIcon={<PublishIcon />}
+                            className={classes.uploadButton}
+                          >
+                            {fileData === "empty" ? "Upload" : "Change"}{" "}
+                            document
+                          </Button>
+                        </label>
+                      </Grid>
+                      <Grid item>{displayFileName()}</Grid>
+                    </Grid>
+                    <div className={classes.submitBtnWrapper}>
+                      <Grid
+                        item
+                        container
+                        md={12}
+                        className={classes.actions}
+                        spacing={2}
                       >
-                        Request Verification
-                      </Button>
-                    </Grid>
-                    <Grid item>
-                      <Button type="reset" fullWidth className={classes.cancel}>
-                        Cancel
-                      </Button>
-                    </Grid>
-                  </Grid>
+                        <Grid item>
+                          <Button
+                            fullWidth
+                            type="submit"
+                            className={classes.button}
+                          >
+                            Request Verification
+                          </Button>
+                          {loading && (
+                            <CircularProgress
+                              size={24}
+                              className={classes.buttonProgress}
+                            />
+                          )}
+                        </Grid>
+                        <Grid item>
+                          <Button
+                            type="reset"
+                            fullWidth
+                            className={classes.cancel}
+                          >
+                            Cancel
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    </div>
+                  </form>
                 </Grid>
               </Grid>
             </Grid>
-          </form>
+          </Grid>
         </Grid>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="edit-details-form"
-          fullWidth
-          className={classes.dialogBox}
-        >
-          <DialogTitle id="edit-details-form">Are you sure?</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              You are going to permanently delete your account from
-              Flamingo.com. Please confirm to delete your account.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button
-              type="submit"
-              onClick={deleteUser}
-              color="primary"
-              className={classes.dialogbuttons}
-            >
-              Confirm and Delete
-            </Button>
-          </DialogActions>
-        </Dialog>
       </Grid>
     </Grid>
   );
