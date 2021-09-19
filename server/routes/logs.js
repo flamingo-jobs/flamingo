b9f622fcc5cefc2b95f71bc10df2e0efd2b25c55
yeshanGunawardana <64021514+yeshanGune@users.noreply.github.com>
2021-09-19 12:41:06 +0530

    user sign in log

--- server/routes/logs.js
+++ server/routes/logs.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const logsController = require('../controllers/logsController');
 
 router.post('/logs/create/:jobId/:userId', logsController.createLogsForJobs);
+router.post('/logs/user', logsController.userSignInLogs);
 
 module.exports = router;
