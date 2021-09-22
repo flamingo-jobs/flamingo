ab226ee59e9b6a4a828deadbab5c87622e52e06a
Thatz Weerakoon <ffutry123@gmail.com>
2021-09-22 17:47:58 +0530

    Update Subscriptions.js

--- client/src/admin/Subscriptions.js
+++ client/src/admin/Subscriptions.js
@@ -3,44 +3,44 @@ import GridTable from "./components/GridTable";
 
 function Subscriptions() {
   const columns = [
-    { field: "type", headerName: "Name", flex: 1, editable: true },
-    { field: "maxJobs", headerName: "Max. Jobs", flex: 1, editable: true },
+    { field: "type", headerName: "Name", width: 150, editable: true },
+    { field: "maxJobs", headerName: "Max. Jobs", width: 150, editable: true },
     {
       field: "maxResumes",
       headerName: "Max. Resumes",
-      flex: 1,
+      width: 150,
       editable: true,
     },
-    { field: "maxUsers", headerName: "Max. Users", flex: 1, editable: true },
+    { field: "maxUsers", headerName: "Max. Users", width: 150, editable: true },
     {
       field: "applicantFiltering",
       headerName: "Applicant Filtering",
-      flex: 1,
+      width: 150,
       editable: true,
       type: "boolean",
     },
     {
       field: "shortlisting",
       headerName: "Applicant Shortlisting",
-      flex: 1,
+      width: 150,
       editable: true,
       type: "boolean",
     },
     {
       field: "customizedShortlisting",
       headerName: "Customized Shortlisting",
-      flex: 1,
+      width: 150,
       editable: true,
       type: "boolean",
     },
     {
       field: "jobSpecificShortlisting",
       headerName: "Job Specific Shortlisting",
-      flex: 1,
+      width: 150,
       editable: true,
       type: "boolean",
     },
-    { field: "price", headerName: "Price(LKR)", flex: 1, editable: true },
+    { field: "price", headerName: "Price (LKR)", width: 150, editable: true },
   ];
 
   return <GridTable columns={columns} type="subscriptions" />;
