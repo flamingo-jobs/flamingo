da3aeb5001693a7159e387886cd28f1ee0be2f6a
Thatz Weerakoon <ffutry123@gmail.com>
2021-10-23 10:22:28 +0530

    Update Config.js

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,7 +1,8 @@
-const BACKEND_URL = "http://52.146.45.183:8000";
-const FRONTEND_URL = "http://52.146.45.183:3000";
+const BACKEND_URL = "http://localhost:8000";
+const FRONTEND_URL = "http://localhost:3000";
+const PAYMENT_URL = "http:/localhost:3000";
 const FILE_URL = "https://flamingofiles.blob.core.windows.net";
 
 
 export default BACKEND_URL;
-export { FILE_URL, FRONTEND_URL };
+export { FILE_URL, FRONTEND_URL, PAYMENT_URL };
